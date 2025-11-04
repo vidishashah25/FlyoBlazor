a554afce2cfa7e9d37bee63341bb9ba9ede059a7
vidishashah25 <vidisha.shah14@gmail.com>
2025-11-04 15:07:17 +0100

    darkmode with blazor

---
+++ Services/ThemeState.cs
@@ -0,0 +1,8 @@
+﻿namespace FlyoBlazor2.Services
+{
+    public class ThemeState
+    {
+        public bool IsDark { get; set; }
+        public Func<Task>? Toggle { get; set; }
+    }
+}
--- obj/Debug/net8.0/FlyoBlazor2.AssemblyInfo.cs
+++ obj/Debug/net8.0/FlyoBlazor2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlyoBlazor2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac19501ae31bc77a0621b11a50e7c374eaf02453")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlyoBlazor2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlyoBlazor2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
